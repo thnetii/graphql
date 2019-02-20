854eb592271dc3f784decf08fa25e1edd36d09fb
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-02-20 16:32:46 +0100

    Add solution, new project, Common submodule and dot-files

---
+++ src/THNETII.GraphQL.FragmentGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+using GraphQLDotnet = GraphQL;
+
+namespace THNETII.GraphQL.FragmentGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
